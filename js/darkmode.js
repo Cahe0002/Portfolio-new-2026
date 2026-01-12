c2ef8d78a2f94be3032d9eb23f9872b0a47a1192
Caroline S <cahe0002@stud.ek.dk>
2026-01-12 01:34:09 +0100

    added darkmode to website

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,5 +1,5 @@
 const html = document.querySelector("html");
-const btn = document.querySelector("#theme-btn");
+const btn = document.querySelector("#toggle");
 
 function toggleTheme() {
   html.classList.toggle("dark");
